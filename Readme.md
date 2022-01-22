13f8d32ae82d3c218c4cbe32dd438631fa534358
thhuynh7 <thhuynh7@myseneca.ca>
2022-01-21 22:35:35 -0500

    added Readme.md

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Project Overview
+
+In this project, I built a backend API for WEB422's Assignment 1.
+
+# Project Demo
+
+To run a demo of this project, please follow these steps.
+
+In the project directory, install node packages using the command:
+
+```
+npm install
+```
+
+Finally, you can run the app in development mode with:
+
+```
+npm run dev
+```
+
+Local server is now running on port:
+
+```
+http://localhost:5000
+```
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Thai Hoa Huynh Student ID: 114968191 Date: 21/01/2022
+ *  Heroku Link: https://backendforweb422.herokuapp.com/api/
+ *
+ ********************************************************************************/
 const express = require('express');
 const dotenv = require('dotenv');
 
