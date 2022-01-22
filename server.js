28bcef3df251ff9eea5d4007beacc3d033138c54
thhuynh7 <thhuynh7@myseneca.ca>
2022-01-21 20:38:08 -0500

    2nd

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ message: 'API Listening' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
