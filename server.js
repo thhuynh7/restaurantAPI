1bb91e5d50861c958866fabb6577392823b67f51
thhuynh7 <thhuynh7@myseneca.ca>
2022-01-21 20:54:42 -0500

    3rd

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Testing
 app.get('/', (req, res) => {
   res.json({ message: 'API Listening' });
+});
+
+// Add routes
+app.post('/api/restaurants', (req, res) => {
+  res.status(200).json({ success: true, message: 'Create a new restaurant' });
+});
+
+app.get('/api/restaurants', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all restaurants' });
+});
+
+app.get('/api/restaurants/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Show restaurant ${req.params.id}` });
+});
+
+app.put('/api/restaurants/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Update restaurant ${req.params.id}` });
+});
+
+app.delete('/api/restaurants/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Delete restaurant ${req.params.id}` });
 });
 
 const PORT = process.env.PORT || 5000;
